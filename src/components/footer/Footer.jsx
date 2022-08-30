ada34798c0df43870a9547e50604c5de75b96339
YashNaj <yashalnajeeb@gmail.com>
2022-08-30 16:35:47 -0700

    new file:   src/components/footer/Footer.jsx new file:   src/components/footer/footer.scss 	new file:   src/styles/responsiveStyles.scss added files

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import './footer.scss';
+const Footer = () => {
+  return (
+    <div className="footer ">
+      <div className="logo"> Hey what the hell</div>
+      <div className="footer-links-container">
+        <a href="" className="footer-email footer-links"></a>
+        <a href="" className="footer-phone footer-links"></a>
+        <a href="" className="footer-adress footer-links"></a>
+      </div>
+    </div>
+  );s
+};
+
+export default Footer;
